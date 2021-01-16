c870b810483469045ff23f2d7206bdd231db8ffc
Al Augustin <al@alaugustin.com>
2021-01-16 15:31:56 -0500

    update score board for all roll conditions

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let win = false;
+let win = false,
+    scoreBoard = document.getElementById("scoreBoard");
 
 // GENERATE RANDOM NUMBER
 function randNum() {
@@ -31,7 +32,8 @@ function ceeLo(a, b, c) {
     function trips() {
 
         if (a === b && b === c) {
-            console.log("Trips - " + a + b + c + "Win is = " + win);
+            console.log("Trips - " + a + b + c + ". Win is = " + win);
+            scoreBoard.innerHTML = "Trips - " + a + b + c;
 
         } else {
             point();
@@ -40,22 +42,20 @@ function ceeLo(a, b, c) {
 
     // POINT CONDITION
     function point() {
-        
-        let scoreBoard = document.getElementById("scoreBoard");
 
         if (a === b) {
             console.log("point - " + c);
-            document.getElementById("scoreBoard").innerHTML = c;
+            scoreBoard.innerHTML = c;
             console.log("Win is = " + win);
 
         } else if (a === c) {
             console.log("point - " + b);
-            document.getElementById("scoreBoard").innerHTML = b;
+            scoreBoard.innerHTML = b;
             console.log("Win is = " + win);
 
         } else if (b === c) {
             console.log("point - " + a);
-            document.getElementById("scoreBoard").innerHTML = a;
+            scoreBoard.innerHTML = a;
             console.log("Win is = " + win);
 
         } else {
@@ -69,18 +69,21 @@ function ceeLo(a, b, c) {
         if (a === 4 && b === 5 && c === 6 || a === 4 && b === 6 && c === 5) {
             win = true;
             console.log("Win is = " + win + ". Instant win.");
+            scoreBoard.innerHTML = "Win - " + a + b + c;
             
             resetGame();
 
         } else if (a === 6 && b === 4 && c === 5 || a === 6 && b === 5 && c === 4) {
             win = true;
             console.log("Win is = " + win + ". Instant win.");
+            scoreBoard.innerHTML = "Win - " + a + b + c;
             
             resetGame();
 
         } else if (a === 5 && b === 6 && c === 4 || a === 5 && b === 4 && c === 6) {
             win = true;
             console.log("Win is = " + win + ". Instant win.");
+            scoreBoard.innerHTML = "Win - " + a + b + c;
             
             resetGame();
 
@@ -94,15 +97,19 @@ function ceeLo(a, b, c) {
 
         if (a === 1 && b === 2 && c === 3 || a === 1 && b === 3 && c === 2) {
             console.log("Win is = " + win + ". Instant loss.");
+            scoreBoard.innerHTML = a + b + c;
 
         } else if (a === 3 && b === 1 && c === 2 || a === 3 && b === 2 && c === 1) {
             console.log("Win is = " + win + ". Instant loss.");
+            scoreBoard.innerHTML = a + b + c;
 
         } else if (a === 2 && b === 3 && c === 1 || a === 2 && b === 1 && c === 3) {
             console.log("Win is = " + win + ". Instant loss.");
+            scoreBoard.innerHTML = a + b + c;
 
         } else {
             console.log("Roll again. Win is = " + win);
+            scoreBoard.innerHTML = "Null";
         }
     }
 
@@ -112,17 +119,14 @@ function ceeLo(a, b, c) {
 // -------------------- PLAYER --------------------
 const rollButtonArray = document.getElementsByTagName("button");
 
+let players = document.getElementsByClassName("playerHolder").length;
+
+// console.log(players);
+
 for (let index = 0; index < rollButtonArray.length; index++) {
-    const element = rollButtonArray[index];
+    const playerRoll = rollButtonArray[index];
 
-    console.log(element);
-
-    element.onclick = function () {
+    playerRoll.onclick = function () {
         randNum();
-    };
-    
+    };    
 }
-
-rollButtonArray.onclick = function () {
-    randNum();
-};
