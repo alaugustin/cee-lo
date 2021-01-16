5a0ef86db614d1cbf1c7c60170763762a2ce65c5
Al Augustin <al@alaugustin.com>
2021-01-16 14:19:47 -0500

    basic win loss conditions

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const roll = document.getElementById("rollButton");
-const LOSS = '123';
-const WIN = '456';
+
+let win = false;
 
 roll.onclick = function () {
     randNum();
@@ -12,10 +12,9 @@ function randNum() {
         a = Math.floor(Math.random() * sides) + 1,
         b = Math.floor(Math.random() * sides) + 1,
         c = Math.floor(Math.random() * sides) + 1;
-
-    // var array = [a, b, c];
+        
     printDice(a, b, c);
-    compare(a, b, c);
+    ceeLo(a, b, c);
 }
 
 // PRINT DICE NUMBERS
@@ -25,15 +24,21 @@ function printDice(a, b, c) {
     document.getElementById("die3").innerHTML = c;
 }
 
-function compare(a, b, c) {
-    // console.log("die1 is " + a, "die2 is " + b, "die3 is " + c);
 // RESET GAME
+function resetGame() {
+    win = false;
+    console.log("Start over.");
+}
+
 // CeeLo DICE ROLLS
+function ceeLo(a, b, c) {
+
     // TRIPS CONDITION
     function trips() {
 
         if (a === b && b === c) {
-            console.log("trips - " + a + b + c);
+            console.log("Trips - " + a + b + c + "Win is = " + win);
+
         } else {
             point();
         }
@@ -41,16 +46,24 @@ function compare(a, b, c) {
 
     // POINT CONDITION
     function point() {
+        
         let scoreBoard = document.getElementById("scoreBoard");
+
         if (a === b) {
             console.log("point - " + c);
             document.getElementById("scoreBoard").innerHTML = c;
+            console.log("Win is = " + win);
+
         } else if (a === c) {
             console.log("point - " + b);
             document.getElementById("scoreBoard").innerHTML = b;
+            console.log("Win is = " + win);
+
         } else if (b === c) {
             console.log("point - " + a);
             document.getElementById("scoreBoard").innerHTML = a;
+            console.log("Win is = " + win);
+
         } else {
             fourFiveSix();
         }
@@ -58,12 +71,25 @@ function compare(a, b, c) {
 
     // 4 5 6 CONDITION
     function fourFiveSix() {
+
         if (a === 4 && b === 5 && c === 6 || a === 4 && b === 6 && c === 5) {
-            console.log("Winner 456");
+            win = true;
+            console.log("Win is = " + win + ". Instant win.");
+            
+            resetGame();
+
         } else if (a === 6 && b === 4 && c === 5 || a === 6 && b === 5 && c === 4) {
-            console.log("Winner 456");
+            win = true;
+            console.log("Win is = " + win + ". Instant win.");
+            
+            resetGame();
+
         } else if (a === 5 && b === 6 && c === 4 || a === 5 && b === 4 && c === 6) {
-            console.log("Winner 456");
+            win = true;
+            console.log("Win is = " + win + ". Instant win.");
+            
+            resetGame();
+
         } else {
             oneTwoThree();
         }
@@ -71,14 +97,18 @@ function compare(a, b, c) {
 
     // 1 2 3 CONDITION
     function oneTwoThree() {
+
         if (a === 1 && b === 2 && c === 3 || a === 1 && b === 3 && c === 2) {
-            console.log("Loser 123");
+            console.log("Win is = " + win + ". Instant loss.");
+
         } else if (a === 3 && b === 1 && c === 2 || a === 3 && b === 2 && c === 1) {
-            console.log("Loser 123");
+            console.log("Win is = " + win + ". Instant loss.");
+
         } else if (a === 2 && b === 3 && c === 1 || a === 2 && b === 1 && c === 3) {
-            console.log("Loser 123");
+            console.log("Win is = " + win + ". Instant loss.");
+
         } else {
-            console.log("roll again");
+            console.log("Roll again. Win is = " + win);
         }
     }
 
