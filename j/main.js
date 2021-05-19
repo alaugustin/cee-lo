f4bca2821f2e34e71fc680e80df68e2bfa8585d2
aaug <aaug@caasco.ca>
2021-05-18 23:47:13 -0400

    test

--- j/main.js
+++ j/main.js
@@ -136,6 +136,7 @@ let ceeLoGame = {
 
             let playerScoreBoard = (playerNumber) => {
                 ceeLoGame.storePlayerScore(playerNumber);
+                //test entry
 
             };
 
