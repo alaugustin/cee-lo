15b1dcf9d91f9e538d832ac431338a0bc4ff211b
aaug-caasco <aaug@caasco.ca>
2024-02-13 20:19:21 -0500

    begining StartGame

--- src/Modules/StartGame.ts
+++ src/Modules/StartGame.ts
@@ -1,3 +1,10 @@
+const startButton = document.getElementById('startButton');
+
+const startGame = () => {
+  console.log('startButton clicked')
+}
+
 export function StartGame() {
+  startButton.addEventListener("click", startGame);
   console.log(`startGame code here`);
 };
