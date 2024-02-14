82e4368c3831e8944c706f8f58e54017ea03b6a1
Al Augustin <al@alaugustin.com>
2024-02-14 03:58:46 +0000

    config updates

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'src'),
     },
     liveReload: true,
     compress: true,
