458aebc7f37b82eb7b0c7f7331c841c5ffb93372
Al Augustin <al@alaugustin.com>
2024-02-14 05:06:43 +0000

    cleaning up player sign-in | begin roll for positon

--- src/Modules/PlayerSignIn.ts
+++ src/Modules/PlayerSignIn.ts
@@ -1,37 +1,21 @@
 
 import { RollForPosition } from "./RollForPosition";
-import { PlayersRoll } from "./PlayersRoll";
-import { CompareScores } from "./CompareScores";
-import { ShowWinner } from "./ShowWinner";
 
 const playerSignInInput = document.getElementById("nameInput");
 const playButton = document.getElementById("playButton");
 
-let players = {
-  player1 : {
-    name: 'House'
-  },
-  player2 : {
-    name: 'UNCHANGED DATA'
-  }
-}
-
-const player2Name = (playerName: string) => {
-  return players.player2.name = playerName;
-};
-
 const logPlayer = (event: any) => {
   let { value } = event.target.parentElement.children[1];
 
-  value ? player2Name(value) : player2Name('Nameless Ned');
+  // value ? player2Name(value) : player2Name('Nameless Ned');
+
+  RollForPosition(
+    "The House",
+    value ? value : "Nameless Ned"
+  );
 };
 
 export function PlayerSignIn() {
   playerSignInInput?.focus();
   playButton?.addEventListener("click", logPlayer);
-
-  RollForPosition('testing -');
-  PlayersRoll('testing -');
-  CompareScores('testing -');
-  ShowWinner('testing -');
 };
