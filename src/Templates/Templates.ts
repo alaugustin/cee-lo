4ae30f61e230c0ce4936d1ecba2b27469b9cae7d
Al Augustin <al@alaugustin.com>
2024-02-15 19:20:16 +0000

    updating card UI | overall UI edits adding Tailwind CDN

--- src/Templates/Templates.ts
+++ src/Templates/Templates.ts
@@ -1,9 +1,21 @@
 export function ScoreboardCard(index: number, name: string, rollPoints: number) {
+  const commonClass: string = "flex flex-col";
+  const firstCard: string = "items-end pr-2";
+  const lastCard: string = "items-start pl-2";
+
+  let divOpenTag: string;
+
+  if (index === 0) {
+    divOpenTag = `<div class="${commonClass} ${firstCard}">`
+  } else {
+    divOpenTag = `<div class="${commonClass} ${lastCard}">`
+  }
+
   return (`
-    <div>
-      <h3>Player ${index + 1}</h3>
-      <h4>${name}</h4>
-      <p>Roll Points: <span>${rollPoints}</span></p>
+    ${divOpenTag}
+      <h3 class="text-3xl">Player ${index + 1}</h3>
+      <h4 class="text-2xl">${name}</h4>
+      <p>Roll Points: <span class="font-bold text-2xl">${rollPoints}</span></p>
       <button class="border-solid border-2">ROLL</button>
     </div>
   `)
