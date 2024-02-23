d933ce746ed27f06d27741b3fd7c0e982aa46b07
Al Augustin <al@alaugustin.com>
2024-02-23 20:30:41 +0000

    refactoring too add playerdata

--- src/Modules/RollForPosition/PopulatePlayerBoard.ts
+++ src/Modules/RollForPosition/PopulatePlayerBoard.ts
@@ -1,11 +1,15 @@
 import { PlayerObj } from '../../Classes/CreatePlayers';
-import { RollForPosition } from './RollForPosition';
 import { ScoreboardCard } from '../../Templates/Templates';
+import { PlayersRoll } from '../PlayersRoll';
+import { CompareScores } from '../CompareScores';
+import { ShowWinner } from '../ShowWinner';
+import { RollDice } from '../Global';
 
 const scoreBoardHolder: HTMLElement | null = document.getElementById('scoreBoardHolder');
 
 export function PopulatePlayerBoard(players: string[]) {
   const reversedPlayersArray: string[] = players.reverse();
+  const playerDataArray: any[] = [];
 
   reversedPlayersArray.forEach((playerName: string, index: number) => {
     const player: PlayerObj = new PlayerObj();
@@ -16,10 +20,25 @@ export function PopulatePlayerBoard(players: string[]) {
     player.wins = null;
     player.losses = null;
 
+    playerDataArray.push(player);
+
     return (
       scoreBoardHolder.innerHTML += ScoreboardCard(index, player.name, player.rollPoints)
     );
   });
 
-  RollForPosition();
+  const scoreboardButtons: NodeListOf<Element> = document.querySelectorAll('#scoreBoardHolder button');
+
+  console.log(playerDataArray);
+
+  scoreboardButtons.forEach((button: HTMLElement) => {
+    button.addEventListener('click', (event: Event | any) => {
+
+      RollDice(event);
+    });
+  });
+
+  PlayersRoll('testing -');
+  CompareScores('testing -');
+  ShowWinner('testing -');
 }
