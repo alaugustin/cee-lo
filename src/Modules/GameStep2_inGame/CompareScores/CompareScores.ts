896af5b2d631e145fdeb0becc4f9bd69b6676abd
Al Augustin <al.augustin@gmail.com>
2024-11-08 18:33:36 +0000

    fix: handle tie events and update player score properties
    - Added `handleTieEvent` function to increment tie count and log tie events in `CompareScores.ts`
    - Updated `CompareScores` function to use `win` instead of `wins` for player score properties
    - Removed commented-out code in `CompareScores.ts`

--- src/Modules/GameStep2_inGame/CompareScores/CompareScores.ts
+++ src/Modules/GameStep2_inGame/CompareScores/CompareScores.ts
@@ -8,19 +8,17 @@ const determineWinner = (playerWin: ICompareScoresProps, playerLoss: ICompareSco
   ToggleGameScreen('endScreen', false);
 }
 
+const handleTieEvent = (player: ICompareScoresProps) => {
+  player.tie += 1;
+  console.log('tie - no winner');
+  console.log(player);
+}
+
 export function CompareScores(playerDataArray: ICompareScoresProps[]) {
-  const player1 = { ...playerDataArray[0], wins: 0, rollPosition: 1 };
-  const player2 = { ...playerDataArray[1], wins: 0, rollPosition: 2 };
+  const player1 = { ...playerDataArray[0], win: 0, rollPosition: 1 };
+  const player2 = { ...playerDataArray[1], win: 0, rollPosition: 2 };
 
   DisableAllButtons();
-
-  // console.log(`
-  //   - player 2 button roll
-  //     • 456: player immediately wins
-  //     • trips: store point to compare player 1 point and player 2 point
-  //     • points: store point to compare player 1 point and player 2 point
-  //     • 123: player 1 instant loss
-  // `);
 
   if (player1.rollCode > player2.rollCode) {
     determineWinner(player1, player2);
@@ -36,10 +34,11 @@ export function CompareScores(playerDataArray: ICompareScoresProps[]) {
       determineWinner(player2, player1);
 
     } else {
-      console.log('tie - roll again, stay in same round');
-      // console.log('first player rolls again');
-      // console.log('Game round: ', GlobalGameData.gameRound);
-      // console.log('Player data: ', playerDataArray);
+      ToggleGameScreen('gameboard', true);
+      handleTieEvent(player1);
+      handleTieEvent(player2);
+      ToggleGameScreen('endScreen', false);
+
     }
   }
 }
